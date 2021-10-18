70d4d953fdbb3ca473cd5e180d51463c94e794d3
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-10-18 11:39:27 -0700

    fix event listeners to have an anonymous function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -13,14 +13,14 @@ var xAndO = function(box) {
   // }
 }
 
-document.getElementById("box1").addEventListener('click', xAndO("box1"))
-document.getElementById("box2").addEventListener('click', xAndO("box2"))
-document.getElementById("box3").addEventListener('click', xAndO("box3"))
+document.getElementById("box1").addEventListener("click", () => {xAndO("box1")}) //callback anonymous or bind
+document.getElementById("box2").addEventListener("click", () => {xAndO("box2")})
+document.getElementById("box3").addEventListener("click", () => {xAndO("box3")})
 
-document.getElementById("box4").addEventListener('click', xAndO("box4"))
-document.getElementById("box5").addEventListener('click', xAndO("box5"))
-document.getElementById("box6").addEventListener('click', xAndO("box6"))
+document.getElementById("box4").addEventListener("click", () => {xAndO("box4")})
+document.getElementById("box5").addEventListener("click", () => {xAndO("box5")})
+document.getElementById("box6").addEventListener("click", () => {xAndO("box6")})
 
-document.getElementById("box7").addEventListener('click', xAndO("box7"))
-document.getElementById("box8").addEventListener('click', xAndO("box8"))
-document.getElementById("box9").addEventListener('click', xAndO("box9"))
+document.getElementById("box7").addEventListener("click", () => {xAndO("box7")})
+document.getElementById("box8").addEventListener("click", () => {xAndO("box8")})
+document.getElementById("box9").addEventListener("click", () => {xAndO("box9")})
