18f31804eaa3fe3c57bc5b05918d0996ae6560fb
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-10-18 11:42:50 -0700

    alternating x and o

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,26 +1,24 @@
 console.log('is this working?')
 
+count = 0;
+
 
 var xAndO = function(box) {
-  document.getElementById(box).innerHTML = 'X';
-  // count = 0;
-  // if (count % 2 === 0) {
-  //   document.getElementById(box).innerHTML = 'O';
-  //   count++;
-  // } else {
-  //   document.getElementById(box).innerHTML = 'O';
-  //   count++;
-  // }
+  if (count % 2 === 0) {
+    document.getElementById(box).innerHTML = 'X';
+  } else {
+    document.getElementById(box).innerHTML = 'O';
+  }
 }
 
-document.getElementById("box1").addEventListener("click", () => {xAndO("box1")}) //callback anonymous or bind
-document.getElementById("box2").addEventListener("click", () => {xAndO("box2")})
-document.getElementById("box3").addEventListener("click", () => {xAndO("box3")})
+document.getElementById("box1").addEventListener("click", () => {xAndO("box1"); count++;}) //callback anonymous or bind
+document.getElementById("box2").addEventListener("click", () => {xAndO("box2"); count++;})
+document.getElementById("box3").addEventListener("click", () => {xAndO("box3"); count++;})
 
-document.getElementById("box4").addEventListener("click", () => {xAndO("box4")})
-document.getElementById("box5").addEventListener("click", () => {xAndO("box5")})
-document.getElementById("box6").addEventListener("click", () => {xAndO("box6")})
+document.getElementById("box4").addEventListener("click", () => {xAndO("box4"); count++;})
+document.getElementById("box5").addEventListener("click", () => {xAndO("box5"); count++;})
+document.getElementById("box6").addEventListener("click", () => {xAndO("box6"); count++;})
 
-document.getElementById("box7").addEventListener("click", () => {xAndO("box7")})
-document.getElementById("box8").addEventListener("click", () => {xAndO("box8")})
-document.getElementById("box9").addEventListener("click", () => {xAndO("box9")})
+document.getElementById("box7").addEventListener("click", () => {xAndO("box7"); count++;})
+document.getElementById("box8").addEventListener("click", () => {xAndO("box8"); count++;})
+document.getElementById("box9").addEventListener("click", () => {xAndO("box9"); count++;})
